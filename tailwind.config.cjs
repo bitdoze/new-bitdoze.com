b42eea31e41c78b7cc311ac23f000f625bf77be8
Dragos Balota <dragos.balota@oracle.com>
2023-03-14 13:11:07 +0200

    add

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,4 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
