221fae1cc6a6f30e8283aa3711f988b086972afa
Dragos Balota <dragos.balota@oracle.com>
2023-06-14 14:42:13 +0300

    updated dependency and small fixes

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { AstroComponentFactory } from 'astro/dist/runtime/server';
+
 export interface Post {
   id: string;
   slug: string;
@@ -20,7 +22,7 @@ export interface Post {
   tags?: Array<string>;
   author?: string;
 
-  Content: unknown;
+  Content: AstroComponentFactory;
   content?: string;
 
   readingTime?: number;
